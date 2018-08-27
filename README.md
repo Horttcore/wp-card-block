76eda450b972118fa3d62108ff2deef8c7e237fa
Ralf Hortt <me@horttcore.de>
2018-08-27 23:14:49 +0200

    Initial release
    Signed-off-by: Ralf Hortt <me@horttcore.de>

---
+++ README.md
@@ -0,0 +1,20 @@
+# WP Card Block
+
+A WordPress Gutenberg card block
+
+## Features
+
+* Image
+* Headline
+* Body
+
+## Translations
+
+* English
+* German
+
+## Changelog
+
+### v0.1.0
+
+* Initial release
---
+++ plugin.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name:     WordPress Card Block
+ * Plugin URI:      https://horttcore.de
+ * Description:     A WordPress Gutenberg Card Block
+ * Author:          Ralf Hortt
+ * Author URI:      https://horttcore.de
+ * Text Domain:     wp-card-block
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Horttcore/WP-Card-Block
+ */
+
+namespace Horttcore\WPCardBlock;
+
+//  Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+/**
+ * Enqueue block script and backend stylesheet.
+ */
+add_action( 'enqueue_block_editor_assets', function() {
+	wp_enqueue_script( 'wp-card-block', plugins_url( 'assets/js/editor.blocks.js', __FILE__ ), [ 'wp-blocks', 'wp-element' ] );
+	wp_enqueue_style( 'wp-card-block-editor', plugins_url( 'assets/css/editor.blocks.css', __FILE__ ) );
+} );
+
+/**
+ * Enqueue styles for backend and frontend.
+ */
+add_action( 'enqueue_block_assets', function() {
+	wp_enqueue_style( 'wp-card-block-frontend', plugins_url( 'assets/css/frontend.blocks.css', __FILE__ ) );
+} );
+
+/**
+ * Load translation
+ */
+add_action( 'plugins_loaded', function(){
+	load_plugin_textdomain( 'wp-card-block', false, plugin_dir_path( __FILE__ ) . 'languages' );
+});
